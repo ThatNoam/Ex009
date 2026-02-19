bce0b1ed4f995a8197c21b466e7b3108fc222a72
ThatNoam <noamk574@gmail.com>
2026-02-19 19:52:56 +0200

    Third commit

--- app/src/main/java/com/example/ex009/MainActivity.java
+++ app/src/main/java/com/example/ex009/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ex009;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -22,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
 
         et = findViewById(R.id.editTextNumber);
         btn = findViewById(R.id.button);
+    }
+
+    public void clicked(View view)
+    {
+        text = et.getText().toString();
+        num = Integer.parseInt(text);
+
+        if ((num % 7) == 0)
+        {
+            btn.setText("BOOM!");
+            et.setText("");
+        }
+
+        else
+        {
+            btn.setText(text);
+            et.setText("");
+        }
     }
 
 }
